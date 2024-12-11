babdabc2a0a8718a8d075d00f89aac47fc214d56
ThyTree-OG <luis.afonso1998@gmail.com>
2024-12-11 19:02:50 +0000

    Criacao de powerpoint

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-<br><br>    
+
 <div class="container">
+<br><br> 
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
